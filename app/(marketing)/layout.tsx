f4f0a558be307dbea1a87cee1ad82e8a0ffa411f
Mark Andrew <jftmain@gmail.com>
2024-03-01 07:20:55 +0200

    changes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,20 @@
 import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
+import backgroundImage from "public/house-7497001.svg";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
       <Navbar />
-      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <main
+        style={{
+          backgroundImage: `url(${backgroundImage.src})`,
+          backgroundSize: "cover",
+        }}
+        className="pt-40 pb-20 bg-slate-100"
+      >
+        {children}
+      </main>
       {/* <Footer /> */}
     </div>
   );
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
-import localFont from "next/font/local";
-import { Poppins } from "next/font/google";
-import { Medal } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 import {
   Carousel,
@@ -13,30 +21,84 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Rebuild } from "./_components/rebuild";
 
 const MarketingPage = () => {
   return (
-    <div className="flex items-center justify-center flex-col w-full">
-      <Carousel>
-        <CarouselContent>
-          <CarouselItem>
-            <Rebuild />
-          </CarouselItem>
-          <CarouselItem>
-            <Rebuild />
-          </CarouselItem>
-          <CarouselItem>
-            <Rebuild />
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+    <div className="flex items-center justify-center flex-col w-full h-[696px]">
+      <div className="flex items-center justify-center flex-col gap-4 w-full h-full">
+        <div
+          className={cn(
+            "text-sm md:text-xl text-blacks mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+            textFont.className
+          )}
+        >
+          After the 7th of October people were displaced. Many want to return
+          home. You can help them.
+        </div>
+        <div className="flex items-center justify-center flex-col gap-4 w-full h-full">
+          <Carousel>
+            <CarouselContent>
+              <CarouselItem>
+                <div
+                  className={cn(
+                    "flex w-full items-center justify-center flex-col",
+                    headingFont.className
+                  )}
+                >
+                  <div className="text-12xl md:text-9xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+                    Restore
+                  </div>
+                </div>
+              </CarouselItem>
+              <CarouselItem>
+                <div
+                  className={cn(
+                    "flex w-full items-center justify-center flex-col",
+                    headingFont.className
+                  )}
+                >
+                  <div className="text-12xl md:text-9xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+                    Rebuild
+                  </div>
+                </div>
+              </CarouselItem>
+              <CarouselItem>
+                <div
+                  className={cn(
+                    "flex w-full items-center justify-center flex-col",
+                    headingFont.className
+                  )}
+                >
+                  <div className="text-12xl md:text-9xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+                    Revive
+                  </div>
+                </div>
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
 
-      <Button className="mt-6" size="lg" asChild>
-        <Link href="/sign-up">Let&apos;s get started!</Link>
-      </Button>
+      <div className="flex gap-4">
+        <Button
+          className="mt-6 bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white"
+          size="lg"
+          asChild
+        >
+          <Link href="/board/404d6779-9e87-444b-bcc7-e13708d63b23">
+            Volunteer
+          </Link>
+        </Button>
+        <Button
+          className="mt-6 bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white"
+          size="lg"
+          asChild
+        >
+          <Link href="/sign-up">Donate</Link>
+        </Button>
+      </div>
     </div>
   );
 };
