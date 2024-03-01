162f8d38932de0c55e56aa6d7380a2ad0a8de645
Chekinm <120395978+Chekinm@users.noreply.github.com>
2024-03-01 04:47:58 +0200

    mm

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -36,7 +36,7 @@ export const Info = ({
           {organization?.name}
         </p>
         <div className="flex items-center text-xs text-muted-foreground">
-          <CreditCard className="h-3 w-3 mr-1" />
+          {/* <CreditCard className="h-3 w-3 mr-1" /> */}
           {isPro ? "Pro" : "Free"}
         </div>
       </div>
