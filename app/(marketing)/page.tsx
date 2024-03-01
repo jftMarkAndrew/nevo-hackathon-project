3241a9366627da7d92c4e807323e5d4ec85342b2
Mark Andrew <jftmain@gmail.com>
2024-03-01 02:19:48 +0200

    nevo

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -34,24 +34,22 @@ const MarketingPage = () => {
       )}>
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
-          No 1 task managment
+          Rebuilding Communities.
         </div>
-        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-          Taskify helps team move
-        </h1>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">Let&apos;s help Shlomo and Shila.</h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-          work forward.
+        Rebuild the Kibutzim.
         </div>
       </div>
       <div className={cn(
         "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
         textFont.className,
       )}>
-        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+        After the 7th of October a lot of people were displaced from the South of Israel. They want to return home and they want to feel that they still have home. And YOU can help them.
       </div>
       <Button className="mt-6" size="lg" asChild>
         <Link href="/sign-up">
-          Get Taskify for free
+          Let&apos;s get started!
         </Link>
       </Button>
     </div>
--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
-  name: "Taskify",
-  description: "Collaborate, manage projects, and reach new productivity peaks",
+  name: "Healing Israel.",
+  description: "Helping rebuild the communities and souls.",
 };
